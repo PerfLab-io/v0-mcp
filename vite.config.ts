c7b3a02fceea002d91ca60a95bf5fb4343f043a6
Vinicius Dallacqua <dev.webx@gmail.com>
2025-07-15 01:53:51 +0200

    Trying again...

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import devServer from "@hono/vite-dev-server";
 import build from "@hono/vite-build/vercel";
 
-export default defineConfig({
-  define: {
-    "process.env.DATABASE_URL": JSON.stringify(process.env.DATABASE_URL),
-  },
-  plugins: [
-    devServer({
-      entry: "src/index.ts",
-    }),
-    build({
-      entry: "./src/index.ts",
-      minify: false,
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "process.env.DATABASE_URL": JSON.stringify(env.DATABASE_URL),
+    },
+    plugins: [
+      devServer({
+        entry: "src/index.ts",
+      }),
+      build({
+        entry: "./src/index.ts",
+        minify: false,
+      }),
+    ],
+  };
 });
