89a5ea55467491aec6fefcaff8ca3fdd392fde72
Vinicius Dallacqua <dev.webx@gmail.com>
2025-07-15 00:13:17 +0200

    Nope

--- api/index.ts
+++ api/index.ts
@@ -30,7 +30,7 @@ const sseControllers = new Map<
   ReadableStreamDefaultController<Uint8Array>
 >();
 
-const app = new Hono<Env>();
+const app = new Hono<Env>().basePath("/api");
 
 app.use(
   "*",
